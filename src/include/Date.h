65ecabe2d721154d95cf33bb68fd3180ddab1177
zam-cv <a01799283@tec.mx>
2023-06-10 15:42:26 -0600

    the date and its definition were placed

---
+++ src/include/Date.h
@@ -0,0 +1,19 @@
+#ifndef DATE_H
+#define DATE_H
+
+class Date {
+private:
+  int day;
+  int month;
+  int year;
+
+public:
+  Date();
+  Date(int, int, int);
+  int getDay();
+  int getMonth();
+  int getYear();
+  std::string toString();
+};
+
+#endif
