bb76a85e3fc39789841d3eb57ac4d7589600d49d
zam-cv <a01799283@tec.mx>
2023-06-11 11:08:30 -0600

    another way to remove boilerplate code

--- src/Platform.cpp
+++ src/Platform.cpp
@@ -1,82 +1,126 @@
 #include "./include/Platform.h"
 
+#include <fstream>
 #include <iostream>
 #include <string>
 
-static const std::string title = "Plataforma de Streaming";
+static const std::string TITLE = "Plataforma de Streaming";
+static const std::string DEFAULT_DATA_FILE_PATH = "../DatosPeliculas.csv";
 
-Platform::Platform() { this->isInvalid = false; }
+Platform::Platform() {
+  this->menuOptions = new Option[this->MENU_OPTIONS_SIZE]{
+      {"Cargar archivo de datos", [&]() { this->loadFile(); }},
+      {"Buscar video", [&]() { this->searchVideo(); }},
+      {"Buscar serie", [&]() { this->searchSerie(); }},
+      {"Buscar pelicula", [&]() { this->searchMovie(); }},
+      {"Calificar video", [&]() { this->rateVideo(); }},
+      {"Salir", [&]() { exit(0); }}};
+  
+  this->fileLoadOptions = new Option[this->FILE_LOAD_OPTIONS_SIZE]{
+      {"Intentar nuevamente", [&]() { this->loadFile(); }},
+      {"Regresar al Menu", [&]() { this->menu(); }}};
+
+  this->isInvalid = false;
+}
 
 void Platform::run() {
   while (true) {
-    this->clear();
     this->showTitle();
     this->menu();
   }
 }
 
-char Platform::getOption() {
-  char option;
+void Platform::showOptions(std::function<void()> showMessage, Option *handlers, int size) {
+  std::string optionStr;
+  int option;
 
-  std::cout << (this->isInvalid ? "\033[31m❯ \033[0m" : "❯ ");
-  std::cin >> option;
+  while (true) {
+    this->showTitle();
+    showMessage();
 
-  return option;
+    for (int i = 0; i < size; i++) {
+      std::cout << i + 1 << ". " << handlers[i].name << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << (this->isInvalid ? "\033[31m❯ \033[0m" : "❯ ");
+    std::cin >> optionStr;
+
+    try {
+      int optionInt = std::stoi(optionStr);
+      optionInt--;
+
+      if (optionInt >= 0 && optionInt < size) {
+        option = optionInt;
+        break;
+      }
+    } catch (std::invalid_argument const &e) {
+    } catch (std::out_of_range const &e) {
+    }
+
+    this->isInvalid = true;
+  }
+
+  this->isInvalid = false;
+  this->showTitle();
+  handlers[option].handle();
 }
 
 void Platform::menu() {
-  std::cout << "1. Cargar archivo de datos" << std::endl
-            << "3. Buscar video" << std::endl
-            << "4. Buscar serie" << std::endl
-            << "5. Buscar pelicula" << std::endl
-            << "6. Calificar video\n\n";
-
-  char option = this->getOption();
-  this->isInvalid = false;
-
-  this->clear();
-  this->showTitle();
-
-  switch (option) {
-  case '1':
-    this->loadFile();
-    break;
-  case '2':
-    std::cout << "Buscar video" << std::endl;
-    std::cin.ignore();
-    break;
-  case '3':
-    std::cout << "Buscar serie" << std::endl;
-    std::cin.ignore();
-    break;
-  case '4':
-    std::cout << "Buscar pelicula" << std::endl;
-    std::cin.ignore();
-    break;
-  case '5':
-    std::cout << "Calificar video" << std::endl;
-    std::cin.ignore();
-  default:
-    this->isInvalid = true;
-    break;
-  }
+  this->showOptions([]() { 
+    std::cout << "Menu\n\n";
+  }, this->menuOptions, this->MENU_OPTIONS_SIZE);
 }
 
 void Platform::loadFile() {
   std::string path;
 
-  std::cout << "Cargar archivo de datos\n\n";
+  std::cout << "Carga del archivo de datos\n\n";
+  std::cout << "Archivo a usar si se presiona enter: " << DEFAULT_DATA_FILE_PATH
+            << std::endl;
   std::cout << "Ingrese la direccion del archivo: ";
 
   std::cin.ignore();
   std::getline(std::cin, path);
 
-  std::cout << "path: " << path << std::endl;
+  if (path.empty()) {
+    path = DEFAULT_DATA_FILE_PATH;
+  }
+
+  std::ifstream file(path);
+
+  if (!file.is_open()) {
+    this->showOptions([]() { 
+      std::cout << "\033[31mError al abrir el archivo\033[0m\n\n";
+    }, this->fileLoadOptions, this->FILE_LOAD_OPTIONS_SIZE);
+  } else {
+    
+  }
+}
+
+void Platform::searchVideo() {
+  std::cout << "Buscar video\n\n";
+  std::cin.ignore();
+}
+
+void Platform::searchSerie() {
+  std::cout << "Buscar serie\n\n";
+  std::cin.ignore();
+}
+
+void Platform::searchMovie() {
+  std::cout << "Buscar pelicula\n\n";
+  std::cin.ignore();
+}
+
+void Platform::rateVideo() {
+  std::cout << "Calificar video\n\n";
   std::cin.ignore();
 }
 
 void Platform::showTitle() {
-  std::cout << "\033[34m======\033[0m " << title
+  this->clear();
+  std::cout << "\033[34m======\033[0m " << TITLE
             << " \033[34m======\033[0m\n\n";
 }
 
--- src/include/Platform.h
+++ src/include/Platform.h
@@ -3,18 +3,34 @@
 
 #include <string>
 
+struct Option {
+  std::string name;
+  std::function<void()> handle;
+};
+
 class Platform {
 private:
+  static const int MENU_OPTIONS_SIZE = 6;
+  static const int FILE_LOAD_OPTIONS_SIZE = 2;
+
+  Option *menuOptions;
+  Option *fileLoadOptions;
+
   bool isInvalid;
+  void clear();
+  void menu();
+  void showTitle();
+  void showOptions(std::function<void()> showMessage, Option *handlers, int size);
+  void showOptions(std::string message, Option *handlers, int size);
+  void loadFile();
+  void searchVideo();
+  void searchSerie();
+  void searchMovie();
+  void rateVideo();
 
 public:
   Platform();
   void run();
-  void clear();
-  void menu();
-  void showTitle();
-  char getOption();
-  void loadFile();
 };
 
 #endif
